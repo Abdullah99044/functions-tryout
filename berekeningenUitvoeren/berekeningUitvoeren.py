b6e3ab9a80d8c6b8af9b0a3c04da1fcee897fcb8
Abdullah <ab-benkathir@outlook.com>
2021-10-19 17:14:57 +0200

    Programma 2: Berekeningen uitvoeren

---
+++ berekeningenUitvoeren/berekeningUitvoeren.py
@@ -0,0 +1,45 @@
+def additionFunction(number1, number2):
+    return number1 + number2
+number = 10
+number1 =  12
+print(number , "+" , number1 , "=" , additionFunction(number , number1))
+
+def subtractionFunction(number1, number2):
+    return number1 - number2
+
+number2 =  58
+number3 = 34
+
+print(number2 , "-" , number3 , "=" ,subtractionFunction(number2 , number3))
+
+def multiplicationFunction(number1, number2):
+    return number1 * number2
+
+number4 =  6
+number5 =  7
+
+print(number4 , "x" , number5 , "=" , multiplicationFunction(number4 , number5))
+
+def divisionFunction(number1, number2):
+    return number1 / number2
+
+number6 =  144
+number7 = 12
+
+print(number6 , "/" , number7 , "=" , divisionFunction(number6 , number7))
+
+def incrementFunction(number1):
+    return number1 + 1
+
+number8 = 12
+
+print(number8 , "+" , "1" , "=" , incrementFunction(number8))
+
+def decrementFunction(number1):
+    return number1 - 1
+
+number9 = 34 
+
+print(number9 , "-" , "1" , "=" , incrementFunction(number9))
+
+ 
